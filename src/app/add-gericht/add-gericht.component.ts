6d6d838053c6423897af740cf0cf21ab5bf9cd37
caophil <philippcao60@gmail.com>
2023-03-24 15:55:33 +0100

    Gerichte hinzufügen funktioniert

--- src/app/add-gericht/add-gericht.component.ts
+++ src/app/add-gericht/add-gericht.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {Kategorie} from './Kategorie';
+import { AuthService } from '../shared/auth.service';
+import { Router } from '@angular/router';
+import { AppHttpClient } from '../shared/http-client.service';
+import { firstValueFrom } from 'rxjs';
+
 
 @Component({
   selector: 'app-add-gericht',
@@ -12,8 +17,24 @@ export class AddGerichtComponent{
     newGericht: {name: string, preis: number, kategorie: Kategorie } = {name:"", preis:0.0, kategorie:0};
 
     
+    constructor(
+      private readonly http: AppHttpClient,
+      private readonly router: Router,
+      private readonly authService: AuthService,
+    ) {}
 
+      async navigateBack(){
+        this.router.navigate(['/adminportal'])
+      }
 
- 
+      async addGericht(){
+        try{
+          const result = await firstValueFrom(this.http.post<any>("/gericht", this.newGericht, {withCredentials: true}))
+          alert(result.name)
+        }catch(error)
+        {
+            alert((error as Error).message)
+        }
+      }
 
 }
