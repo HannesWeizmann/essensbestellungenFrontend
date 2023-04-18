fb701ce8f6f49d47de45de40b25e5532b1f57620
HannesWeizmann <hannesweizmann54@gmail.com>
2023-04-18 16:01:40 +0200

    Preise werden in Speiseplan angezeigt

--- src/app/speiseplan/speiseplan.component.ts
+++ src/app/speiseplan/speiseplan.component.ts
@@ -38,44 +38,6 @@ export class SpeiseplanComponent implements OnInit {
 
   private auth: Auth = new Auth();
   public username = "";
-  tage = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'];
-  selectedTag: string = '';
-  selectedMenu: string = '';
-  hasNachtisch = false;
-  hasSuppe = false;
-  isVegetarisch = false;
-
-  //variablen nicht final nur zum designen
-  public datum = "14.04.2023-21.04.2023";
-  public menu1_montag = "Nudeln a la Bolognese (Schwein/Rind) mit Käse";
-  public menu2_montag = "Maultaschen mit Kartfoffelsalat";
-  public suppe_montag = "Schokoladenpudding";
-  public nachtisch_montag = "Nudelsuppe";
-
-  public menu1_dienstag = "Kesselgulasch mit Brötchen";
-  public menu2_dienstag = "Tiroler Burger mit Pommes";
-  public suppe_dienstag = "Dänische KnusperCreme";
-  public nachtisch_dienstag = "Bratknödelsuppe";
-  
-  public menu1_mittwoch = "Eisbeinfleisch mit Sauerkraut & Erbsenpüree";
-  public menu2_mittwoch = "Pasta mit frischem Gemüse und einr Tomaten-Mozarellasauce";
-  public suppe_mittwoch = "Obstsalat";
-  public nachtisch_mittwoch= "Kartoffelsuppe";
-
-  public menu1_donnerstag = "Dönerteller mit Pommes und Salat";
-  public menu2_donnerstag = "Grießbrei mit heißen Früchten";
-  public suppe_donnerstag = "Himbeer-Mascarpone Dessert";
-  public nachtisch_donnerstag = "Gemüsesuppe";
-
-  public menu1_freitag = "Schnitzel mit Rahmchampions & Rosmarinkartoffeln";
-  public menu2_freitag = "Paniertes Seelachsfilet mit Remouladensoße und Bratkartoffeln";
-  public suppe_freitag = "Vanilleeis mit Fürchten";
-  public nachtisch_freitag = "Grießklößchensuppe";
-
-  public menu1_samstag = "";
-  public menu2_samstag = "Würstchengulasch mit Nudeln";
-  public suppe_samstag = "Erdbeerkuchen";
-  public nachtisch_samstag = "";
 
   async naechsteWoche(){
     this.montag.setDate(this.montag.getDate()+ 7);
@@ -97,43 +59,6 @@ export class SpeiseplanComponent implements OnInit {
     this.samstag.setDate(this.samstag.getDate()- 7);
     this.sonntag.setDate(this.sonntag.getDate()- 7);
     this.matchDays();
-  }
-  
-  submitBestellung() {
-    if (!this.selectedTag) {
-      alert("Bitte wählen Sie einen Tag aus.");
-      return;
-    }
-    
-  // automatisch Menü 2 auswählen und Suppe deaktivieren, wenn Samstag ausgewählt wurde
-  if (this.selectedTag === 'Samstag') {
-    this.selectedMenu = 'Menü 2';
-    this.hasSuppe = false;
-  } else if (!this.selectedMenu) {
-    alert("Bitte wählen Sie ein Menü aus.");
-    return;
-  }
-    
-    // Erstellen eines Bestellungs-Objekts
-    const bestellung = {
-      tag: this.selectedTag,
-      menu: this.selectedMenu || null,
-      nachtisch: this.hasNachtisch,
-      suppe: this.hasSuppe,
-      vegetarisch: this.isVegetarisch
-    };
-    
-    //Code, der die Bestellung an den Server sendet oder lokal speichert
-    // ...
-  
-    // Rücksetzen des Formulars
-    this.selectedTag = '';
-    this.selectedMenu = '';
-    this.hasNachtisch = false;
-    this.hasSuppe = false;
-    this.isVegetarisch = false;
-  
-    alert("Ihre Bestellung wurde erfolgreich aufgegeben.");
   }
 
   ngOnInit(): void {
@@ -185,6 +110,50 @@ export class SpeiseplanComponent implements OnInit {
     this.freitag.setDate(this.montag.getDate()+ 4);
     this.samstag.setDate(this.montag.getDate()+ 5);
     this.sonntag.setDate(this.montag.getDate()+ 6);
+  }
+
+
+  tage = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'];
+  selectedTag: string = '';
+  selectedMenu: string = '';
+  hasNachtisch = false;
+  hasSuppe = false;
+  isVegetarisch = false;
+  submitBestellung() {
+    if (!this.selectedTag) {
+      alert("Bitte wählen Sie einen Tag aus.");
+      return;
+    }
+    
+  // automatisch Menü 2 auswählen und Suppe deaktivieren, wenn Samstag ausgewählt wurde
+  if (this.selectedTag === 'Samstag') {
+    this.selectedMenu = 'Menü 2';
+    this.hasSuppe = false;
+  } else if (!this.selectedMenu) {
+    alert("Bitte wählen Sie ein Menü aus.");
+    return;
+  }
+    
+    // Erstellen eines Bestellungs-Objekts
+    const bestellung = {
+      tag: this.selectedTag,
+      menu: this.selectedMenu || null,
+      nachtisch: this.hasNachtisch,
+      suppe: this.hasSuppe,
+      vegetarisch: this.isVegetarisch
+    };
+    
+    //Code, der die Bestellung an den Server sendet oder lokal speichert
+    // ...
+  
+    // Rücksetzen des Formulars
+    this.selectedTag = '';
+    this.selectedMenu = '';
+    this.hasNachtisch = false;
+    this.hasSuppe = false;
+    this.isVegetarisch = false;
+  
+    alert("Ihre Bestellung wurde erfolgreich aufgegeben.");
   }
 
 }
