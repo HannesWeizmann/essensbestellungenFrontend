1f8cf0a914864478463a7744781d1e189e340503
HannesWeizmann <hannesweizmann54@gmail.com>
2023-04-21 13:00:29 +0200

    cypress entfernen, erste Testversuche bei sign-up

--- src/app/sign-up/sign-up.component.spec.ts
+++ src/app/sign-up/sign-up.component.spec.ts
@@ -1,17 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Router } from '@angular/router';
+import { firstValueFrom, of } from 'rxjs';
+import { AuthService } from '../shared/auth.service';
+import { AppHttpClient } from '../shared/http-client.service';
 import { SignUpComponent } from './sign-up.component';
 
 describe('SignUpComponent', () => {
   let component: SignUpComponent;
   let fixture: ComponentFixture<SignUpComponent>;
+  let authService: jasmine.SpyObj<AuthService>;
+  let http: jasmine.SpyObj<AppHttpClient>;
+  let router: jasmine.SpyObj<Router>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SignUpComponent ]
-    })
-    .compileComponents();
+    authService = jasmine.createSpyObj('AuthService', ['hasAccessToken']);
+    authService.hasAccessToken.and.returnValue(false);
+    http = jasmine.createSpyObj('AppHttpClient', ['post']);
+    router = jasmine.createSpyObj('Router', ['navigate']);
 
+    await TestBed.configureTestingModule({
+      declarations: [SignUpComponent],
+      providers: [
+        { provide: AuthService, useValue: authService },
+        { provide: AppHttpClient, useValue: http },
+        { provide: Router, useValue: router },
+      ],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(SignUpComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +36,49 @@ describe('SignUpComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to login if user is authenticated', async () => {
+    authService.hasAccessToken.and.returnValue(true);
+
+    await component.ngOnInit();
+
+    expect(router.navigate).toHaveBeenCalledWith(['/login']);
+  });
+
+  it('should sign up user and navigate to login', async () => {
+    http.post.and.returnValue(of({ User: 'test', msg: 'Success' }));
+
+    await component.signUp();
+
+    expect(http.post).toHaveBeenCalledWith(
+      '/users/signup',
+      component.userToSignUp,
+      jasmine.objectContaining({ withCredentials: true })
+    );
+    expect(alert).toHaveBeenCalledWith('Success');
+    expect(router.navigate).toHaveBeenCalledWith(['/login']);
+  });
+
+  it('should show error message if sign up fails', async () => {
+    const errorMessage = 'Server error';
+    http.post.and.throwError(errorMessage);
+
+    await component.signUp();
+
+    expect(http.post).toHaveBeenCalledWith(
+      '/users/signup',
+      component.userToSignUp,
+      jasmine.objectContaining({ withCredentials: true })
+    );
+    expect(alert).not.toHaveBeenCalled();
+    expect(router.navigate).not.toHaveBeenCalled();
+    expect(component.errorMessage).toEqual(errorMessage);
+  });
+
+  it('should navigate back to login', () => {
+    component.navigateBack();
+
+    expect(router.navigate).toHaveBeenCalledWith(['/login']);
   });
 });
