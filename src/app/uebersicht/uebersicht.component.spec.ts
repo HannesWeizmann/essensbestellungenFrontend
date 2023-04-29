3178277cf0b0798b293d8afef1ae2a1157dad830
HannesWeizmann <hannesweizmann54@gmail.com>
2023-04-29 08:52:55 +0200

    Übersicht-test datei funktioniert und cypress import ohne probleme

--- src/app/uebersicht/uebersicht.component.spec.ts
+++ src/app/uebersicht/uebersicht.component.spec.ts
@@ -2,13 +2,24 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UebersichtComponent } from './uebersicht.component';
 
+import { AppHttpClient } from '../shared/http-client.service';
+import { AuthService } from '../shared/auth.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
+
 describe('UebersichtComponent', () => {
   let component: UebersichtComponent;
   let fixture: ComponentFixture<UebersichtComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UebersichtComponent ]
+      declarations: [ UebersichtComponent,
+      MatCard,
+      MatCardHeader,
+      MatCardTitle,
+      MatCardContent ],
+      imports: [ HttpClientTestingModule ],
+      providers: [ AppHttpClient, AuthService ]
     })
     .compileComponents();
 
